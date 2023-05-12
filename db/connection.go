b9be23dab11dd4cb2c72959068b52818b0f13665
suriyaa4055 <jayasuriyaa.1905035@srec.ac.in>
2023-05-12 14:15:51 +0530

    updated database connection for containeriation

--- db/connection.go
+++ db/connection.go
@@ -18,7 +18,7 @@ func Connect() *sql.DB {
 	// dbPass := "root"
 	// dbName := "bookdb"
 
-	db, err := sql.Open(dbDriver, "root:Suriyaa@tcp(127.0.0.1:3306)/demo")
+	db, err := sql.Open(dbDriver, "root:Suriyaa@tcp(host.docker.internal:3306)/demo")
 	//db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@tcp(mysql:3306)/"+dbName)
 	if err != nil {
 		//log.Fatal(err.Error())
